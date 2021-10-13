020a28d146163c144ac0d15f74a31bd13b02ce33
Gnprasad2 <76981773+Gnprasad2@users.noreply.github.com>
2021-10-13 15:52:31 +0530

    Update index.js

--- index/index.js
+++ index/index.js
@@ -452,12 +452,12 @@ function googleTranslateElementInit() {
     'hi,as,bh,bn,dz,gu,kn,ks,ml,mr,ne,or,pa,te,ta,ur,sa,sd,en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
   }
 
-  window.onload = (event) => {
+  document.onload = (event) => {
     M.AutoInit();
     
-//     if(window.location == window.top.location) {
-//       window.location = "https://script.google.com/macros/s/AKfycbyaV7YNwZLEfpRagpgkMctfJPrwjACjoOP5fmumjcc/dev";
-//     }
+    if(window.location == window.top.location) {
+      window.location = "https://script.google.com/macros/s/AKfycbyaV7YNwZLEfpRagpgkMctfJPrwjACjoOP5fmumjcc/dev";
+    }
     
     
    
